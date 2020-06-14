d818fa94288282372af622ca1a7700ae57f07170
n24651 <cristobal.dabed@nrk.no>
2020-06-15 00:47:03 +0200

    TypeScript version for recursive pow implementation

---
+++ src/ts/recursion/pow.ts
@@ -0,0 +1,17 @@
+
+// https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+export default function pow (x: number, n: number):number {
+  // initial condition
+  if (n === 1) {
+    return x
+  }
+
+  // even number
+  if ((n % 2) === 0) {
+    return pow(x * x, n / 2)
+  }
+
+  // odd number
+  return x * pow(x *x, (n -1) / 2)
+}
+
