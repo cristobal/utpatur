6ec91a428e52f6334686a800436831769f02e2e0
n24651 <cristobal.dabed@nrk.no>
2020-06-28 09:56:36 +0200

    Added clojure version of pow

---
+++ src/clojure/math/pow.clj
@@ -0,0 +1,12 @@
+(defn pow [x n]
+  (cond
+    ; initial condition
+    (= 1 n) x
+
+    ; even number
+    (= (rem n 2) 0) (pow (* x x) (/ n 2))
+
+    ; odd number
+    :else (* x (pow (* x x) (/ (- n 1) 2)))
+  )
+)
